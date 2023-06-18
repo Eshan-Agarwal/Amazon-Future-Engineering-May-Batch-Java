7e74a6794591094d4c7e38b842b97f0856ab6b26
Eshan-Agarwal <eshanagarwal12@gmail.com>
2023-06-18 12:02:49 +0530

    Number System Complete & Arrays basics

---
+++ Lecture10/Arrays.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+public class Arrays {
+
+    public static void main(String[] args) {
+
+//        intArr();
+        inpurInArr();
+    }
+
+    public static void inpurInArr() {
+
+        Scanner scn = new Scanner(System.in);
+
+        int size = scn.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        printArr(arr);
+
+    }
+
+    public static void printArr(int[] arr) {
+
+        // for loop
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+        // for-each loop
+        for (int val: arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+
+    }
+
+    public static void intArr() {
+
+        // creation of arr
+        int[] arr = new int[6];
+
+        // Addtion in arr
+        // 1st position --> 0th idx
+        arr[0] = 10;
+        // 2nd position --> 1st idx
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        arr[5] = 60;
+//        arr[6] = 70; // ---> Error is arrayIndexOutOfBounds
+
+        int val = arr[0];
+        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
+
+        // Update elements
+        System.out.println("Before Update: " + arr[2]);
+        arr[2] = 200;
+        System.out.println("After Update: " + arr[2]);
+
+        // Default Values of cell in arr --> 0
+        System.out.println("Default values of cell in arr");
+        int[] defaultArr = new int[10];
+        System.out.println(defaultArr[8]);
+        System.out.println(defaultArr[4]);
+
+        // lenght of arr
+        System.out.println("Len of arr: " + arr.length);
+        System.out.println("Len of defaultArr: " + defaultArr.length);
+
+        // iteration in arr
+        System.out.println("Loops in arrays");
+        System.out.println("Using for loops in arr");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("Iteration in defaultArr");
+        for (int i = 0; i < defaultArr.length; i++) {
+            System.out.print(defaultArr[i] + " ");
+        }
+
+        // while loops
+        System.out.println("Using while loops in arr");
+        int idx = 0;
+        while(idx < arr.length) {
+            System.out.print(arr[idx] + " ");
+            idx++;
+        }
+        System.out.println();
+
+
+    }
+
+}
